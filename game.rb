bfe3622d58754d60e537b2befab2aff2a03f1bd7
Kate Connolly <katherine.d.connolly@gmail.com>
2018-12-04 22:54:55 +0000

    created game loop

--- game.rb
+++ game.rb
@@ -1,25 +1,26 @@
 class Game
+    def initialize(player1, player2)
+        @player1 = player1
+        @player2 = player2
+    end 
 
-
-puts "\n> Welcome to the Math Game! \n>\n> Player 1, press 's' to begin."
-
-print ">"
-start = $stdin.gets.chomp
-
-if start == "s"
-    int_1 = rand(20)
-    int_2 = rand(20)
-    puts "Player 1: What does #{int_1} plus #{int_2} equal?"
-
-        print ">"
-        answer1 = $stdin.gets.chomp.to_i
-
-        if answer1 = (int_1 + int_2)
-            puts "Yes! You are correct!"
-        else 
-            puts "Seriously? No!"
+    def game_loop(turn)
+        while @player1.lives > 0 && @player2.lives > 0
+            turn.question(@player1)
+            puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+            puts "---- NEW TURN ----"
+            if @player1.lives > 0 
+                turn.question(@player2)
+                puts "P1: #{player1.lives}/3 vs P2: #{player2.lives}/3"
+                puts "---- NEW TURN ----"
         end
-        
-        else
-            puts "Thanks for player, come back soon!!"
+
+        if @player1.lives === 0
+            puts "Player 2 wins with a score of #{player2.lives}/3"
+            puts "---- GAME OVER ----"
+            puts "Goodbye!"
+        end
+
     end
+
+end
--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
-require '/player.rb'
-require '/game.rb'
-require 'turn.rb'
+require './players.rb'
+require './game.rb'
+require './turn.rb'
 
 player_1 = Player.new("Player 1")
 player_2 = Player.new("Player 2")
 game_1 = Game.new(player_1, player_2)
 turn = Turn.new
 
+p game_1.game_loop(turn)
